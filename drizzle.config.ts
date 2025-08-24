02ddf673576d0ad74668903a75307697dde5d1d8
manas <66103885+manasbir@users.noreply.github.com>
2025-08-24 02:44:18 -0400

    mv drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: './src/drizzle',
+  schema: './src/drizzle/schema.ts',
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
